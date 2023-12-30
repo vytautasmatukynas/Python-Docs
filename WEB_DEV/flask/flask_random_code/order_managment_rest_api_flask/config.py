3c6de8e8aac7ee63fa479b5fccfaddfc7ac8ec4c
Vytautas <51360361+vytautasmatukynas@users.noreply.github.com>
2023-12-30 17:26:03 +0200

    Delete WEB_DEV/flask/flask_random_code/order_managment_rest_api_flask/config.py

--- WEB_DEV/flask/flask_random_code/order_managment_rest_api_flask/config.py
+++
@@ -1,9 +0,0 @@
-""" DB INFO """
-
-sql_db = {"host": '192.168.0.39',
-                "database": 'test_db',
-                "user": 'test_user',
-                "password": 'aaaa',
-                "port": '5432'}
-
-
